09080c4a9be0328fc5239656b14b47cb5569975e
Jonathan Fischoff <jonathangfischoff@gmail.com>
2013-01-01 22:44:59 -0800

    better doc

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-soon soon soon
+Use this package to compute maximum entropy distributions given a list of values and
+list of constraints.
+
+Here is a the example from Probability the Logic of Science
+
+    maxent ([1,2,3], [average 1.5])
+
+    Right [0.61, 0.26, 0.11]
+
+The classic dice example
+
+    maxent ([1,2,3,4,5,6], [average 4.5])
+
+    Right [.05, .07, 0.11, 0.16, 0.23, 0.34]
--- src/MaxEnt.hs
+++ src/MaxEnt.hs
@@ -1,8 +1,18 @@
-{-  | Use this package to compute maximum entropy distributions given a list of values and
-      list of constraints.
-      
-      
--}
+-- |
+-- Use this package to compute maximum entropy distributions given a list of values and
+-- list of constraints.
+-- 
+-- Here is a the example from Probability the Logic of Science
+-- 
+-- > maxent ([1,2,3], [average 1.5])
+-- 
+-- Right [0.61, 0.26, 0.11]
+-- 
+-- The classic dice example
+-- 
+-- > maxent ([1,2,3,4,5,6], [average 4.5])
+-- 
+-- Right [.05, .07, 0.11, 0.16, 0.23, 0.34]
 module MaxEnt (
     Constraint,
     constraint,
